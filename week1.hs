0bfbd38c34c4eeb62b34f3880ebfcbe982efc0bc
Mathis Bullinger <mathis@bullinger.dev>
2022-01-23 13:42:23 +0100

    week 1 exercise 5

--- week1.hs
+++ week1.hs
@@ -2,18 +2,18 @@
 
 toDigits :: Integer -> [Integer]
 toDigits n
-  | n > 0     = toDigits (n `div` 10) ++ [n `mod` 10]
+  | n > 0 = toDigits (n `div` 10) ++ [n `mod` 10]
   | otherwise = []
 
 toDigitsRev :: Integer -> [Integer]
 toDigitsRev n
-  | n > 0     = n `mod` 10 : toDigitsRev(n `div` 10)
+  | n > 0 = n `mod` 10 : toDigitsRev (n `div` 10)
   | otherwise = []
 
 -- exercise 2
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther xs = reverse (zipWith (*) (reverse xs) (cycle [1,2]))
+doubleEveryOther xs = reverse (zipWith (*) (reverse xs) (cycle [1, 2]))
 
 -- exercise 3
 
@@ -24,3 +24,16 @@ sumDigits xs = sum $ map (sum . toDigits) xs
 
 validate :: Integer -> Bool
 validate n = (sumDigits . doubleEveryOther . toDigits) n `mod` 10 == 0
+
+-- exercise 5
+
+type Peg = String
+
+type Move = (Peg, Peg)
+
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+-- hanoi n a b c = [(a, b)]
+
+hanoi 0 a b c = []
+hanoi 1 a b c = [(a, b)]
+hanoi n a b c = hanoi (n - 1) a c b ++ hanoi 1 a b c ++ hanoi (n - 1) c b a
