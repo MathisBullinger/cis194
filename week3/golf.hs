3464485ddf215dfdbb457c8fc14756081adf4666
MathisBullinger <mathis@bullinger.dev>
2022-01-24 21:52:35 +0100

    week 3 exercise 2

--- week3/golf.hs
+++ week3/golf.hs
@@ -2,3 +2,11 @@ module Golf where
 
 skip :: [a] -> [[a]]
 skip x = [[fst e | e<-zip x [1..], mod (snd e) n == 0] | n<-[1..length x]] 
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima x = [b | a:b:c:[] <- chunk x 3, a < b && b > c] 
+
+chunk :: [a] -> Int -> [[a]]
+chunk x n
+    | length x < n = []
+    | otherwise    = take n x : chunk (tail x) n 
