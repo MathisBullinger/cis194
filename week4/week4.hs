61c065acfd4d217e12e81cb452f3edeff3a977ab
MathisBullinger <mathis@bullinger.dev>
2022-01-26 04:15:12 +0100

    week 4 exercise 4

--- week4/week4.hs
+++ week4/week4.hs
@@ -46,3 +46,9 @@ xor = foldr (/=) False
 map' :: (a -> b) -> [a] -> [b]
 map' f = foldr (\a b -> (f a) : b) []
 
+
+-- exercise 4
+
+sieveSundaram :: Integer -> [Integer]
+sieveSundaram n = [2*x+1 | x<-[1..n], 
+                   notElem x [i+j+2*i*j | i<-[1..n], j<-[i..n], i+j+2*i*j<=n]]
