bbe92c8cc8551defefbfc74b4e03555deae560e6
MathisBullinger <mathis@bullinger.dev>
2022-01-27 06:30:11 +0100

    week 5 exercise 4

--- week5/Calc.hs
+++ week5/Calc.hs
@@ -21,3 +21,27 @@ instance Expr ExprT where
   lit = Lit
   add = Add
   mul = Mul
+
+instance Expr Integer where
+  lit = id
+  add = (+)
+  mul = (*) 
+
+instance Expr Bool where
+  lit = (>0)
+  add = (||)
+  mul = (&&)
+
+newtype MinMax = MinMax Integer deriving (Show, Eq, Ord)
+newtype Mod7 = Mod7 Integer deriving (Show, Eq)
+
+instance Expr MinMax where
+  lit = MinMax
+  add = max
+  mul = min
+
+instance Expr Mod7 where
+  lit = Mod7 . (`mod` 7)
+  add (Mod7 a) (Mod7 b) = Mod7 (a + b `mod` 7)
+  mul (Mod7 a) (Mod7 b) = Mod7 (a * b `mod` 7)
+
